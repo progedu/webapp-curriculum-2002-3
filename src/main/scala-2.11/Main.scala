096f5ee315b099deb46226b303dbb1c629f29671
murosan <masashi_16n1200013@nnn.ed.jp>
2017-03-25 14:59:52 +0900

    挑戦の上級

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -1,5 +1,13 @@
 object Main {
 
-  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = ???
+  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = {
+    def review(i: Int): Boolean = {
+      if (i == sortedSeq.length - 1 || sortedSeq.isEmpty) true
+      else if (!ordered(sortedSeq(i), sortedSeq(i + 1))) false
+      else review(i + 1)
+    }
+
+    review(0)
+  }
 
 }
