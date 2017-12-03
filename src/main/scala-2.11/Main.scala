826c4f03f79499ec623e2c6d6acdb81aa12d3b8e
kadokado <kqkpwkado@gmail.com>
2017-12-03 02:38:47 +0000

    関数 isSorted の実装

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -1,5 +1,21 @@
 object Main {
 
-  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = ???
+  // 型をパラメータ化
+  def isSorted[E](sortedSeq: Seq[E])(isOrdered: (E, E) => Boolean): Boolean = {
+  	
+    def isSortedRecursive(i: Int): Boolean = {
+      
+      if (i == sortedSeq.length - 1) true
+      
+      else if (!isOrdered(sortedSeq(i), sortedSeq(i + 1))) false
+
+      // i,i+1の順番どおりであれば、1つ進めて、isSortedRecursive関数(自分自身)呼び出し->（末尾再帰）
+      else isSortedRecursive(i + 1)
+
+    }
+
+    // isSortedRecursive 0からスタート
+    if(sortedSeq.length == 0) true else isSortedRecursive(i = 0)
+  }
 
 }
