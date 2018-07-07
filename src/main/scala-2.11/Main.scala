ceace9180961b0e30289ad7eb580c3b0fb5779a6
nonoike <12398740+nonoike@users.noreply.github.com>
2018-07-07 21:16:23 +0900

    末尾再帰化

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -1,6 +1,9 @@
+import scala.annotation.tailrec
+
 object Main {
 
   def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = {
+    @tailrec
     def f(seq: Seq[E]): Boolean = seq match {
       case Nil | _ :: Nil => true
       case h1 :: h2 :: tail if ordered(h1, h2) => f(h2 :: tail)
