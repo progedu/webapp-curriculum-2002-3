774e7f6c25e83ddac946a127beb60577fcaa240e
nonoike <12398740+nonoike@users.noreply.github.com>
2018-07-07 21:12:17 +0900

    挑戦上級 中身がソートされているかどうかを確認する関数を実装

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -1,5 +1,13 @@
 object Main {
 
-  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = ???
+  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = {
+    def f(seq: Seq[E]): Boolean = seq match {
+      case Nil | _ :: Nil => true
+      case h1 :: h2 :: tail if ordered(h1, h2) => f(h2 :: tail)
+      case _ => false
+    }
+
+    f(sortedSeq)
+  }
 
 }
