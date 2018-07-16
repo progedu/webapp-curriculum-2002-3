70b31fc8867ae3ae5d2c11a81af360bd37b344cd
yukikoya <36853872+yukikoya@users.noreply.github.com>
2018-07-16 18:20:44 +0900

    上級問題

--- src/main/scala-2.11/Main.scala
+++ src/main/scala-2.11/Main.scala
@@ -1,5 +1,13 @@
 object Main {
 
-  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = ???
+def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = {
+    def sortRec(i: Int): Boolean = {
+      if (i == sortedSeq.length - 1) true
+      else if (!ordered(sortedSeq(i), sortedSeq(i + 1))) false
+      else sortRec(i + 1)
+    }
+    sortRec(0)
+ }
 
-}
+  
+  
