d5878122c6efb6ef841b3e4233dae0e4bc6b52c3
208-209 <208_209@outlook.jp>
2019-03-02 01:06:32 +0900

    isSortedメソッドの実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,16 @@
+import scala.annotation.tailrec
+
 object Main {
 
-  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = ???
+  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = {
+
+    @tailrec
+    def go(seq: Seq[E]): Boolean = seq match {
+      case _ :: Nil | Nil => true
+      case x :: xs if !ordered(x, xs.head) => false
+      case _ :: xs => go(xs)
+    }
+    go(sortedSeq)
+  }
 
 }
