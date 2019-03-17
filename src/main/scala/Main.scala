c7d8d4e2fb20e39974f1917617d09c240da8cd55
sdk40010 <sdk40010@gmail.com>
2019-03-17 18:17:32 +0900

    isSorted関数を実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,12 @@
 object Main {
 
-  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = ???
+  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = {
+    def isSortedRec(i: Int): Boolean = {
+      if(sortedSeq.length - 1 == i) true
+      else if (!ordered(sortedSeq(i), sortedSeq(i + 1))) false
+      else isSortedRec(i + 1)
+    }
+    if(sortedSeq.length == 0) true else isSortedRec(0)
+  }
 
 }
