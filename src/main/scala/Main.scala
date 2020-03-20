83a0cfe25c3056b7a5f2e5e0fa9797199968a8e8
QWYNG <ikusawasi@gmail.com>
2020-03-20 18:46:34 +0900

    impl isSorted

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,13 @@
 object Main {
 
-  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = ???
+  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = {
+    def checkOrderRec(i: Int): Boolean = {
+      if(sortedSeq.length == i + 1) true
+      else if (ordered(sortedSeq(i), sortedSeq(i + 1))) checkOrderRec(i + 1)
+      else false
+    }
+
+    checkOrderRec(0)
+  }
 
 }
