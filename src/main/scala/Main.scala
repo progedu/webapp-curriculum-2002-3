44ffa28c8e6823bf8e5c2053dc325ca43cc69d25
YuuTakimotoY <44459676+YuuTakimotoY@users.noreply.github.com>
2020-06-16 23:59:53 +0900

    isSortedの実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,12 +1,11 @@
 object Main {
 
   def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = {
-    def isSorted[E](sortedSeq: Seq[E])(isOrdered: (E, E) => Boolean): Boolean = {
-  def isSortedRecursive(i: Int): Boolean = {
-    if (i == sortedSeq.length - 1) true
-    else if (!isOrdered(sortedSeq(i), sortedSeq(i + 1))) false
-    else isSortedRecursive(i + 1)
+    def isSortedRecursive(i: Int): Boolean = {
+      if (i == sortedSeq.length - 1) true
+      else if (!isOrdered(sortedSeq(i), sortedSeq(i + 1))) false
+      else isSortedRecursive(i + 1)
+    }
+    if(sortedSeq.length == 0) true else isSortedRecursive(0)
   }
-  if(sortedSeq.length == 0) true else isSortedRecursive(0)
-}
 }
