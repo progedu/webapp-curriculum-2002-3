ccead055640618eefe7b1e254e4393bf79075ab3
yrberrytea <yrberrytea@gmail.com>
2021-02-25 19:11:57 +0900

    関数isSortedを実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,11 @@
 object Main {
 
-  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = ???
+  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = {
+    if (sortedSeq.length <= 1) return true
+    for (i <- 0 until sortedSeq.length - 1) {
+      if (!ordered(sortedSeq(i), sortedSeq(i + 1))) return false
+    }
+    true
+  }
 
 }
