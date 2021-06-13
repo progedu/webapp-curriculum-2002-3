43f1a230fc632c0154d0422e79def82d88ef262b
RyujiYamamoto <yamamoto.ryuji.dev@gmail.com>
2021-06-13 20:29:14 +0900

    isSortedの実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,8 @@
 object Main {
 
   def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = {
-    val notSorted = !sortedSeq.iterator.sliding(2).forall(seq => {
+    sortedSeq.iterator.sliding(2).forall(seq => {
       ordered(seq(0), seq(1))
     })
-
-    !notSorted
   }
-
 }
