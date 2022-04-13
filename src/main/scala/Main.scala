170b982a3b77c328fea39345d105518cbec4c0ce
Keito-Mac <keito19981129@gmail.com>
2022-04-14 00:45:45 +0900

    演習の解答を実装

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,20 @@
+import scala.annotation.tailrec
+
 object Main {
 
-  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = ???
+  def isSorted[E](sortedSeq: Seq[E])(ordered: (E, E) => Boolean): Boolean = {
+    @tailrec
+    def checkSort(num: Int): Boolean = {
+      if(num == sortedSeq.length - 1) {
+        return true
+      }
+      if(ordered(sortedSeq(num), sortedSeq(num + 1))) {
+        checkSort(num + 1)
+      } else {
+        return  false
+      }
+    }
+    checkSort(0)
+  }
 
 }
